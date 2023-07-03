95712e314ad6e8f5eeb2dc8766966f7c3b5ae650
AnthonySinitsa <itonyemail@gmail.com>
2023-07-03 19:01:34 +0000

    usedCommand

--- api/howdy.py
+++ api/howdy.py
@@ -3,17 +3,17 @@ from urllib import parse
 
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # parse the quey string
-        path = self.path
-        url_components = parse.urlsplit(path)
-        query_string_list = parse.parse_qsl(url_components.query)
-        dic = dict(query_string_list)
+  def do_GET(self):
+    # parse the quey string
+    path = self.path
+    url_components = parse.urlsplit(path)
+    query_string_list = parse.parse_qsl(url_components.query)
+    dic = dict(query_string_list)
 
-        name = dic.get("name")
+    name = dic.get("name")
 
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(f"Howdy {name}".encode())
-        return
+    self.send_response(200)
+    self.send_header("Content-type", "text/plain")
+    self.end_headers()
+    self.wfile.write(f"Howdy {name}".encode())
+    return
