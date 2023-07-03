7210bee169a72b2599c89a516cb519b593b0a8e4
AnthonySinitsa <itonyemail@gmail.com>
2023-07-03 18:52:26 +0000

    usedCommand

--- api/howdy.py
+++ api/howdy.py
@@ -1,11 +1,19 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+from urllib import parse
+
 
 class handler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(f'Howdy! The time is {datetime.now()}'.encode())
-    return
-  
+    def do_GET(self):
+        # parse the quey string
+        path = self.path
+        url_components = parse.urlsplit(path)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+
+        name = dic.get("name")
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(f"Howdy {name}".encode())
+        return
